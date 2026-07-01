5704dd48996ea8a0ba7795277f3c09d0db02fe16
tuxnode <tuxnode@outlook.com>
2026-07-01 15:47:39 +0800

    refactor: remove legacy duplicate code from encryption.go
    - Remove someUsefulThings() demo function
    - Remove legacy InitUser/GetUser standalone functions
    - Remove legacy User methods (StoreFile, LoadFile, etc.)
    - Remove unused EncryptedData struct
    - Keep only User and Inode struct definitions
    - Update unit tests to use service-based approach

--- internal/client/encryption/encryption_unittest.go
+++ internal/client/encryption/encryption_unittest.go
@@ -1,34 +1,11 @@
 package encryption
 
-///////////////////////////////////////////////////
-//                                               //
-// Everything in this file will NOT be graded!!! //
-//                                               //
-///////////////////////////////////////////////////
-
-// In this unit tests file, you can write white-box unit tests on your implementation.
-// These are different from the black-box integration tests in client_test.go,
-// because in this unit tests file, you can use details specific to your implementation.
-
-// For example, in this unit tests file, you can access struct fields and helper methods
-// that you defined, but in the integration tests (client_test.go), you can only access
-// the 8 functions (StoreFile, LoadFile, etc.) that are common to all implementations.
-
-// In this unit tests file, you can write InitUser where you would write client.InitUser in the
-// integration tests (client_test.go). In other words, the "client." in front is no longer needed.
-
 import (
-	userlib "github.com/cs161-staff/project2-starter-code/internal/userlib"
 	"testing"
-)
 
-import (
-	_ "encoding/hex"
-	_ "errors"
+	userlib "github.com/cs161-staff/project2-starter-code/internal/userlib"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	_ "strconv"
-	_ "strings"
 )
 
 func TestSetupAndExecution(t *testing.T) {
@@ -43,18 +20,40 @@ var _ = Describe("Client Unit Tests", func() {
 		userlib.KeystoreClear()
 	})
 
-	Describe("Unit Tests", func() {
-		Specify("Basic Test: Check that the Username field is set for a new user", func() {
-			userlib.DebugMsg("Initializing user Alice.")
-			// Note: In the integration tests (client_test.go) this would need to
-			// be client.InitUser, but here (client_unittests.go) you can write InitUser.
-			alice, err := InitUser("alice", "password")
+	Describe("UserService", func() {
+		Specify("InitUser sets the Username field correctly", func() {
+			storage := NewMemoryStorage()
+			keyStore := NewMemoryKeyStore()
+			svc := NewUserService(storage, keyStore)
+
+			alice, err := svc.InitUser("alice", "password")
+			Expect(err).To(BeNil())
+			Expect(alice.Username).To(Equal("alice"))
+		})
+
+		Specify("GetUser retrieves an existing user", func() {
+			storage := NewMemoryStorage()
+			keyStore := NewMemoryKeyStore()
+			svc := NewUserService(storage, keyStore)
+
+			_, err := svc.InitUser("alice", "password")
 			Expect(err).To(BeNil())
 
-			// Note: You can access the Username field of the User struct here.
-			// But in the integration tests (client_test.go), you cannot access
-			// struct fields because not all implementations will have a username field.
-			Expect(alice.Username).To(Equal("alice"))
+			user, err := svc.GetUser("alice", "password")
+			Expect(err).To(BeNil())
+			Expect(user.Username).To(Equal("alice"))
+		})
+
+		Specify("GetUser rejects wrong password", func() {
+			storage := NewMemoryStorage()
+			keyStore := NewMemoryKeyStore()
+			svc := NewUserService(storage, keyStore)
+
+			_, err := svc.InitUser("alice", "password")
+			Expect(err).To(BeNil())
+
+			_, err = svc.GetUser("alice", "wrongpassword")
+			Expect(err).ToNot(BeNil())
 		})
 	})
 })
